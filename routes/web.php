eccf9818495457b0630ff09993b625e70e5f87d0
kazuyafudemae <naru.boru.gaoth@gmail.com>
2021-12-13 12:09:51 +0900

    routing修正

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['prefix' => 'cart'], function() {
 	Route::get('index', 'Cart\CartController@index')->name('cart.index');
-	Route::post('', 'Cart\CartController@delete')->name('cart.delete');
+	Route::post('delete', 'Cart\CartController@delete')->name('cart.delete');
 	Route::post('index', 'Cart\CartController@add')->name('cart.add');
 });
 
